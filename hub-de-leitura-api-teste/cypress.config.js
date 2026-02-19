1bea589fb52a67ebc8006e775b4ca62742e4fa63
Anissa <anissa.adv@gmail.com>
2026-02-19 12:56:47 -0300

    automacao de testes api

---
+++ hub-de-leitura-api-teste/cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: true,
+
+  env: {
+    hideCredentials: true
+  
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // eventos aqui se precisar
+    },
+    baseUrl:'http://localhost:3000/api'
+  },
+});
